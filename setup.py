c986733c0b68b929b43e06347df65f19a9484416
TomSmithCGAT <tss38@cam.ac.uk>
2017-05-12 16:58:21 +0100

    adds regex to package dependencies

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ install_requires = [
     "numpy>=1.7",
     "pandas>=0.12.0",
     "future",
-    "six"]
+    "six",
+    "regex"]
 
 # This is a hack. When Pysam is installed from source, the recorded
 # version is 0.2.3, even though a more recent version is actaully
