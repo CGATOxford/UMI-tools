7ffe8cb181f5803954bd99526ee26ae3884cb0a7
k3yavi <avi.srivastava@stonybrook.edu>
2017-08-04 20:37:20 -0400

    Dumping progress

--- umi_tools/extract.py
+++ umi_tools/extract.py
@@ -427,8 +427,9 @@ def main(argv=None):
     else:
         #variable for progress monitor
         progCount = 0
-        displayMax = 500000
-       
+        displayMax = 100000
+        print("Starting barcode extraction")
+
         read2s = umi_methods.fastqIterate(U.openFile(options.read2_in))
 
         if options.read2_out:
@@ -441,12 +442,13 @@ def main(argv=None):
 
         for read1, read2 in umi_methods.joinedFastqIterate(
                 read1s, read2s, strict):
-            # incrementing count for monitoring progress    
+            # incrementing count for monitoring progress
             progCount += 1
-            # Update display in every 500kth iteration
+            # Update display in every 100kth iteration
             if (progCount % displayMax == 0):
-                print("\r Parsed {} reads".format(progCount))
-              
+                print("\r Parsed {} reads".format(progCount)),
+                sys.stdout.flush()
+
             reads = ReadExtractor(read1, read2)
 
             if options.reads_subset:
