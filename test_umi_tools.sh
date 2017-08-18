bcd032a812c89762c049d83a91e0eab0544780ff
TomSmithCGAT <tss38@cam.ac.uk>
2017-08-18 14:06:24 +0100

    limits travis OSX testing to installation and running --help on tools

--- test_umi_tools.sh
+++ test_umi_tools.sh
@@ -8,6 +8,13 @@ if [ $TEST_STYLE ] ; then
     nosetests -v tests/test_style.py ;
 elif [ $TEST_FUNCTIONALITY ] ; then
     nosetests -v tests/test_umi_tools.py ;
+elif [ $TEST_HELP ] ; then
+    umi_tools whitelist --help;
+    umi_tools extract --help;
+    umi_tools group --help;
+    umi_tools dedup --help;
+    umi_tools count --help;
+    umi_tools count_tab --help;
 fi
 
 
