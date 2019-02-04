e1bcbbbf1fccf826b0004a174b8678f69842a3a5
Tom Smith <tss38@cam.ac.uk>
2019-02-04 09:25:50 +0000

    Update count.py (#314)

--- umi_tools/count.py
+++ umi_tools/count.py
@@ -218,7 +218,7 @@ def main(argv=None):
     for event in bundle_iterator.read_events.most_common():
         U.info("%s: %s" % (event[0], event[1]))
 
-    U.info("Number of reads counted: %i" % nOutput)
+    U.info("Number of (post deduplication) reads counted: %i" % nOutput)
 
     U.Stop()
 
